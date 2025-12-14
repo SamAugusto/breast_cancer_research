9dcd7df20629c7d28cbd40cba2453b17d00a79f4
SamAugusto <SamAugusto121@gmail.com>
2025-12-14 07:34:24 -0500

    saved the distribution results in png and txt for the scores

--- scripts/radius_mean_analysis.py
+++ scripts/radius_mean_analysis.py
@@ -26,8 +26,12 @@ def bar_plot_2_data(normal, affected) -> None: # Takes 2 data column
     return None
 def distribution_analysis(radius_mean_data) -> tuple:
     '''Takes both normal and affected data to analyze if the radius mean have a significant difference'''
+    # Creating and saving distribution histograms
+    fig,ax = plt.subplots()
     sns.histplot(data=radius_mean_data,x="radius_mean",hue="diagnosis",palette=["#88413A","#43A35F"])
-    plt.show()
+    fig.savefig(sys.stdout.buffer,format="png")
+    
+    # Filtering data to compare z-scores
     b_dist_data = radius_mean_data[filter_by_diagnosis(radius_mean_data,"B")]["radius_mean"]
     m_dist_data = radius_mean_data[filter_by_diagnosis(radius_mean_data,"M")]["radius_mean"]
     
@@ -37,12 +41,19 @@ if __name__ == "__main__":
     cancer_data = pd.read_csv("../data/breast_cancer_dataset.csv.gz") # Takes the raw data as an input and returns a dataframe (pandas)
     avg_b_radius_mean_cancer_data = np.average(cancer_data[filter_by_diagnosis(cancer_data,"B")]["radius_mean"])
     avg_m_radius_mean_cancer_data = np.average(cancer_data[filter_by_diagnosis(cancer_data,"M")]["radius_mean"])
+    
+    ######## Analysis 1 understanding the average mean radius and comparing benging vs malign tumors averages########
     # Creates the figure saved in analysis for the "average radius mean comparisson"
     # bar_plot_2_data(avg_b_radius_mean_cancer_data,avg_m_radius_mean_cancer_data)
 
+    
+    ### Analysis 2 visualizing distributions and checking for significant differences ###############
     # choosing the entire distribution instead of just a single average number
     dist_radius_mean_cancer_data = cancer_data[["diagnosis","radius_mean"]]
+   
+
     # Does a distribution analysis of the entire radius mean data of benign and malign tumors
     z,p = distribution_analysis(dist_radius_mean_cancer_data)
-    print(f"Benign vs Malign data: Z-score {z:.2f}, p-value {p:.2f}")
+    # prints the values in radius distribution.txt file
+    # print(f"Benign vs Malign data: Z-score {z:.2f}, p-value {p:.2f}")
 
